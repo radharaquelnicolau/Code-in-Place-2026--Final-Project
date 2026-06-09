712c07c44ba9076684b9a1951ad517500fafe3d9
radharaquelnicolau <radharaquelnicolau@gmail.com>
2026-06-09 20:40:05 +0200

    Imported modules to main and printed Hello World as a test

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import greetings_and_farewell
+import numbers_and_dates
+import directions_and_transportation
+import locations_and_shopping
+import common_phrases
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
